6a89234f042ad732cd7037e2e60f53c96f4dd1ad
Maljaaa <Maljaaa@github.com>
2023-11-20 16:30:00 +0900

    chore(static): backend 프로젝트 환경 설정
    - build.gradle: 라이브러리 추가
    - application.yml: server, spring logging 등 설정

---
+++ src/main/java/com/maljaaa/commerse/CommerseApplication.java
@@ -0,0 +1,13 @@
+package com.maljaaa.commerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maljaaa/commerse/CommerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maljaaa.commerse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
