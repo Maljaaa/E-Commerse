0bd0a30ab7c6fcc32651295d5360b0147db73c07
Maljaaa <Maljaaa@github.com>
2023-11-22 13:26:11 +0900

    refactor(HomeController): log 추가
    - Slf4j를 활용하여 로그 기능 추가

--- src/main/java/com/maljaaa/commerse/controller/HomeController.java
+++ src/main/java/com/maljaaa/commerse/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.maljaaa.commerse.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class HomeController {
 
     @GetMapping("/")
     public String index() {
+        log.info("Home Controller");
         return "index.html";
     }
+
+
 }
